5f613bcb5373d1c08a6ee3cfb27c71f652a61ad9
KARTHIKEYAN.A <karthikeyan.a@live.com>
2018-01-31 09:47:59 +0530

    client file created

---
+++ client.js
@@ -0,0 +1,16 @@
+    var net = require('net');
+    
+    var client = new net.Socket();
+    client.connect(5000, '127.0.0.1', function() {
+    	console.log('Connected');
+    	client.write('{info:"Hello, server! Love, Client"}');
+    });
+    
+    client.on('data', function(data) {
+    	console.log('Received: ' + data);
+    	client.end(); // kill client after server's response
+    });
+    
+    client.on('close', function() {
+    	console.log('Connection closed');
+    });
